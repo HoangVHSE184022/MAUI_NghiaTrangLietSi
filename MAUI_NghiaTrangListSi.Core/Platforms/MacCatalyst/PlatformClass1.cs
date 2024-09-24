57a21739a03032b2aa52105bf57ae0ea42cc0c0e
Võ Huy Hoàng <145216389+HoangVHSE184022@users.noreply.github.com>
2024-09-24 11:07:07 +0700

    add project lib

---
+++ MAUI_NghiaTrangListSi.Core/Platforms/MacCatalyst/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace MAUI_NghiaTrangListSi.Core
+{
+    // All the code in this file is only included on Mac Catalyst.
+    public class PlatformClass1
+    {
+    }
+}
---
+++ MAUI_NghiaTrangListSi.Core/Platforms/iOS/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace MAUI_NghiaTrangListSi.Core
+{
+    // All the code in this file is only included on iOS.
+    public class PlatformClass1
+    {
+    }
+}
