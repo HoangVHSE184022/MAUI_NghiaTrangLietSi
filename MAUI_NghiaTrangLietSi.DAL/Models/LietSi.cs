eae6073e15c9932d4bc0e4a80d620faf7fd60a5c
Hyudeque <quanghuy01062004@gmail.com>
2024-09-30 16:54:46 +0700

    Add Realm and repository

---
+++ MAUI_NghiaTrangLietSi.DAL/Models/LietSi.cs
@@ -0,0 +1,24 @@
+﻿using Realms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUI_NghiaTrangLietSi.DAL.Models
+{
+    public class LietSi : RealmObject
+    {
+        [PrimaryKey]
+        public string LietSiId { get; set; } = Guid.NewGuid().ToString(); 
+        public string HoVaTen { get; set; }   
+        public string QueQuan { get; set; }   
+        public int NamSinh { get; set; }    
+        public int NamMat { get; set; }    
+        public string DonVi { get; set; }   
+        public string CapBac { get; set; }  
+        public string NoiYenNghi { get; set; }
+        public double ViTriMoX { get; set; } 
+        public double ViTriMoY { get; set; }
+    }
+}
---
+++ MAUI_NghiaTrangLietSi.DAL/Repositories/LietSiRepository.cs
@@ -0,0 +1,78 @@
+﻿using MAUI_NghiaTrangLietSi.DAL.Models;
+using Realms;
+
+namespace MAUI_NghiaTrangLietSi.DAL.Repositories
+{
+    public class LietSiRepository
+    {
+        private Realm _realm;
+
+        public LietSiRepository()
+        {
+            RealmConfiguration config = new RealmConfiguration
+            {
+                SchemaVersion = 1,
+                ShouldDeleteIfMigrationNeeded = true
+            };
+
+            _realm = Realm.GetInstance(config);
+        }
+
+        public async Task AddLietSiAsync(LietSi lietSi)
+        {
+            using (var transaction = _realm.BeginWrite())
+            {
+                _realm.Add(lietSi);
+                await transaction.CommitAsync(); 
+            }
+        }
+
+        public async Task<IEnumerable<LietSi>> GetAllLietSisAsync()
+        {
+            return await Task.Run(() => _realm.All<LietSi>().ToList());
+        }
+
+        public async Task<LietSi> GetLietSiByIdAsync(string lietSiId)
+        {
+            return await Task.Run(() => _realm.Find<LietSi>(lietSiId));
+        }
+
+        public async Task UpdateLietSiAsync(LietSi lietSi)
+        {
+            using (var transaction = _realm.BeginWrite())
+            {
+                var lietSiToUpdate = _realm.Find<LietSi>(lietSi.LietSiId);
+
+                if (lietSiToUpdate != null)
+                {
+                    lietSiToUpdate.HoVaTen = lietSi.HoVaTen;
+                    lietSiToUpdate.QueQuan = lietSi.QueQuan;
+                    lietSiToUpdate.NamSinh = lietSi.NamSinh;
+                    lietSiToUpdate.NamMat = lietSi.NamMat;
+                    lietSiToUpdate.DonVi = lietSi.DonVi;
+                    lietSiToUpdate.CapBac = lietSi.CapBac;
+                    lietSiToUpdate.NoiYenNghi = lietSi.NoiYenNghi;
+                    lietSiToUpdate.ViTriMoX = lietSi.ViTriMoX;
+                    lietSiToUpdate.ViTriMoY = lietSi.ViTriMoY;
+                }
+
+                await transaction.CommitAsync();
+            }
+        }
+
+        public async Task DeleteLietSiAsync(string lietSiId)
+        {
+            using (var transaction = _realm.BeginWrite())
+            {
+                var lietSiToDelete = _realm.Find<LietSi>(lietSiId);
+
+                if (lietSiToDelete != null)
+                {
+                    _realm.Remove(lietSiToDelete);
+                }
+
+                await transaction.CommitAsync();
+            }
+        }
+    }
+}
