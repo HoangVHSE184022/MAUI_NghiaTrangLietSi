8e22345b90b39c002c7813c019aa8640eebacb5a
Võ Huy Hoàng <145216389+HoangVHSE184022@users.noreply.github.com>
2024-09-30 21:01:01 +0700

    Reapply "Adding LietSi"
    This reverts commit c7e8a962234a0a9372a1eaa98be5916e5618fb17.

---
+++ MAUI_NghiaTrangLietSi.BLL/Services/LietSiService.cs
@@ -0,0 +1,42 @@
+﻿using MAUI_NghiaTrangLietSi.DAL.Models;
+using MAUI_NghiaTrangLietSi.DAL.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MAUI_NghiaTrangLietSi.BLL.Services
+{
+    public class LietSiService
+    {
+        private readonly LietSiRepository _lietSiRepository;
+
+        public LietSiService()
+        {
+            _lietSiRepository = new LietSiRepository();
+        }
+
+        public async Task AddLietSiAsync(LietSi lietSi)
+        {
+            await _lietSiRepository.AddLietSiAsync(lietSi);
+        }
+
+        public async Task<IEnumerable<LietSi>> GetAllLietSisAsync()
+        {
+            return await _lietSiRepository.GetAllLietSisAsync();
+        }
+
+        public async Task<LietSi> GetLietSiByIdAsync(string lietSiId)
+        {
+            return await _lietSiRepository.GetLietSiByIdAsync(lietSiId);
+        }
+
+        public async Task UpdateLietSiAsync(LietSi lietSi)
+        {
+            await _lietSiRepository.UpdateLietSiAsync(lietSi);
+        }
+
+        public async Task DeleteLietSiAsync(string lietSiId)
+        {
+            await _lietSiRepository.DeleteLietSiAsync(lietSiId);
+        }
+    }
+}
